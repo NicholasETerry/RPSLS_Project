63a9a32a0c587fdc0710062d22f19f808cd0de4d
NicholasETerry <nicholas.e.terry@gmail.com>
2020-12-10 08:47:56 -0600

    added all classes to start coding project

---
+++ RPSLS/Rock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Rock
+    {
+        // MEMBER VARIABLE ( HAVE A )
+
+
+        // CONSTRUCTOR ( SPAWNER )
+
+
+        // MEMBER METHOD ( CAN DO )
+    }
+}
